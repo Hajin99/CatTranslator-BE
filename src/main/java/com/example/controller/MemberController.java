a2223664393920830acac36eb59f0bb8c15e46e8
Hajin <hajin6529@naver.com>
2026-03-28 22:34:35 +0900

    feat: openapi 기반 문서 자동화

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 
 import com.example.apiPayload.ApiResponse;
-import com.example.apiPayload.code.ErrorReasonDTO;
 import com.example.apiPayload.code.status.ErrorStatus;
 import com.example.apiPayload.exception.handler.MemberHandler;
 import com.example.converter.MemberConverter;
@@ -11,8 +10,7 @@ import com.example.domain.enums.Role;
 import com.example.dto.MemberRequestDTO;
 import com.example.dto.MemberResponseDTO;
 import com.example.service.MemberService;
-//import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +26,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    //@Operation(summary = "회원가입 API", description = "회원가입하는 API입니다.")
+    @Operation(summary = "회원가입 API", description = "회원가입하는 API입니다.")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
 
         if (memberService.checkEmailExists(request.getEmail())) {
@@ -52,12 +50,13 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    //@Operation(summary = "유저 로그인 API", description = "유저가 로그인하는 API입니다.")
+    @Operation(summary = "유저 로그인 API", description = "유저가 로그인하는 API입니다.")
     public ApiResponse<MemberResponseDTO.LoginResultDTO> login(@RequestBody @Valid MemberRequestDTO.LoginRequestDTO request) {
         return ApiResponse.of(LOGIN_SUCCESS, memberService.loginUser(request));
     }
 
     @GetMapping("/info")
+    @Operation(summary= "마이페이지 API", description = "사용자 정보 API입니다.")
     public ApiResponse<MemberResponseDTO.MemberInfoDTO> getMyInfo(HttpServletRequest request) {
         return ApiResponse.onSuccess(memberService.getMemberInfo(request));
     }
